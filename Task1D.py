9bbbf698eedc737f2353fe49f663a27c0ccd70e9
Hiral Radia <hr388@cam.ac.uk>
2017-02-02 18:40:39 +0000

    Complete Task 1D with test module updated

---
+++ Task1D.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 17:22:44 2017
+
+@author: Hiral
+"""
+
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_with_station
+from floodsystem.geo import stations_by_river
+
+def run():
+    
+    #build station list
+    stations=build_station_list()
+    
+    #creates sorted set of rivers monitored
+    rivers_monitored=rivers_with_station(stations)
+    
+    #prints the first 10 entries in the set
+    print(rivers_monitored[0:10])
+    
+    #generates dictionary
+    stations_by_river_dict=stations_by_river(stations)
+    
+    #prints dictionary entries for 3 rivers
+    print("\n",stations_by_river_dict['River Aire'])
+    print("\n",stations_by_river_dict['River Cam'])
+    print("\n",stations_by_river_dict['Thames'])
+    
+    
+    
+if __name__ == "__main__":
+    print("*** Task 1D: CUED Part IA Flood Warning System ***")
+
+    # Run Task1D
+    run()
+
+
+    
