740eafaf2d9ff74505a2e4a518ca7e14274a0d8c
Oliver Jones <falcoso@hotmail.co.uk>
2017-03-01 11:58:17 +0000

    put polyfit plot function into plot moducl and modified Task2F accordingly

--- Task2F.py
+++ Task2F.py
@@ -7,13 +7,9 @@ Created on Mon Feb 27 23:05:31 2017
 
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
-from floodsystem.plot import plot_water_levels
+from floodsystem.plot import plot_water_levels, plot_water_levels_with_fit
 from floodsystem.datafetcher import fetch_measure_levels
-from floodsystem.analysis import polyfit
 from datetime import datetime, timedelta
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
 
 def run():
     # Build list of stations
@@ -32,38 +28,18 @@ def run():
             if j[0] == i.name:
                 stations_to_plot.append(i) 
     
-    dt = 2    
+    dt = 10    
     #plot data for each station
     for i in stations_to_plot:
+        #fetch data for each station
+        dates, levels = fetch_measure_levels(i.measure_id, dt=timedelta(days=dt))
         
-        try:
-            #fetch data for each station
-            dates, levels = fetch_measure_levels(i.measure_id,
-                                             dt=timedelta(days=dt))
-            poly, d0 = polyfit(dates, levels, 10)
-            x= matplotlib.dates.date2num(dates)
+        #plot each station
+        plot_water_levels_with_fit(i,dates,levels,10)
         
-            #plot polynomial
-            plt.plot(dates, poly(x - d0), label = "poly fit")
-        
-            #plot original data 
-            plot_water_levels(i.name,dates,levels)
-        
-            #plot low and high levels
-            high_low_ranges = i.typical_range
-            low = [high_low_ranges[0]]*len(dates)
-            high= [high_low_ranges[1]]*len(dates)
-            plt.plot(dates, low, label = "low level")
-            plt.plot(dates, high, label = "high level")
-            plt.legend(loc='upper left')
-        
-            #plt.legend("Bestfit", "Original data", "low level", "high level")
-            plt.show()
-        
-        except:
-            print("{} does not have full data available to plot for the given period".format(i.name))
+    return
             
 if __name__ == "__main__":
-    print("*** Task 2E: CUED Part IA Flood Warning System ***")
+    print("*** Task 2F: CUED Part IA Flood Warning System ***")
 
     run()
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -7,21 +7,46 @@ Created on Mon Feb 27 11:16:32 2017
 
 import matplotlib
 import matplotlib.pyplot as plt
+from floodsystem.analysis import polyfit
 
 def plot_water_levels(station, dates, levels):
     "Inputs: station name, dates of data recording and the river levels at that point."
     "Outputs: Plot of the water levels against time"
     
-    plt.plot(dates, levels)
+    plt.plot(dates, levels, label = "original data")
     
     plt.xlabel('date')
     plt.ylabel('water level (m)')
     plt.xticks(rotation=45);
-    plt.legend(station)
+    plt.title("Water levels for {}".format(station))
 
     # Display plot
     plt.tight_layout()  # This makes sure plot does not cut off date labels
+    
+    return
 
+def plot_water_levels_with_fit(station, dates, levels, p):
+    try:
+       #fetch data for each station
+       poly, d0 = polyfit(dates, levels, 10)
+       x= matplotlib.dates.date2num(dates)
+        
+       #plot polynomial
+       plt.plot(dates, poly(x - d0), label = "poly fit")
+        
+       #plot original data 
+       plot_water_levels(station.name,dates,levels)
+        
+       #plot low and high levels
+       high_low_ranges = station.typical_range
+       low = [high_low_ranges[0]]*len(dates)
+       high= [high_low_ranges[1]]*len(dates)
+       plt.plot(dates, low, label = "low level")
+       plt.plot(dates, high, label = "high level")
+       plt.legend(loc='upper left')
+        
+       #plt.legend("Bestfit", "Original data", "low level", "high level")
+       plt.show()
     
-    
-    
+    except:
+        print("{} does not have full data available to plot for the given period".format(station.name))
